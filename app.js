a4673b4df17ad2542027f3a06e3d095f3cf5868e
Kim Mi Reu <samkmr0220@gmail.com>
2022-10-15 23:44:36 +0900

    add mongo-sanitize

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const ExpressError = require('./utils/ExpressError');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const adminRoutes = require('./routes/admin');
 const userRoutes = require('./routes/users');
@@ -34,6 +35,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 
 const sessionConfig = {
   secret: 'secret',
